c592048246cd04f714c820bb8929823d8defc52f
Oko Djangmah <codjangmah@gmail.com>
2020-02-12 21:41:14 -0600

    ..

---
+++ dbo/Tables/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [EmployeeId] INT            IDENTITY (1, 1) NOT NULL,
+    [Fullname]   NVARCHAR (MAX) NULL,
+    [Department] NVARCHAR (MAX) NULL,
+    [Email] NVARCHAR(100) NULL, 
+    [Phone] NVARCHAR(50) NULL,
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+);
+
